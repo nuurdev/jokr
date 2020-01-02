36a8cb559a08f6fa9910791458570d635049b8b0
nuurdev <nuurdev+github@gmail.com>
2020-01-02 10:44:11 +0000

    Serve client on express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
