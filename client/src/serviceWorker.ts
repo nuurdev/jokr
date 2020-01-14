056b24cfcd403f62c23b2ef27e9a05f40ecfcbd3
nuurdev <52236281+nuurdev@users.noreply.github.com>
2020-01-14 11:46:25 +0000

    Implemented bulma and boomer (#21)

--- client/src/serviceWorker.ts
+++ client/src/serviceWorker.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-use-before-define */
